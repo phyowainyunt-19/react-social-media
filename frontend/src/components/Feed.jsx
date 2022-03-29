9ef8bc8e1f2f3e0c90cb2989baabd77cb80c97f7
PhyoWaiNyunt <50352633+pwn-69@users.noreply.github.com>
2022-03-29 22:15:48 +0630

    initial feed view

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom"; // for param
+
+import { client } from "../client";
+import { feedQuery, searchQuery } from "../utils/data";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const [loading, setLoading] = useState(false);
+  const [pins, setPins] = useState(null);
+  const { categoryId } = useParams();
 
-export default Feed
+  /**
+   * fetch all posts or a single post or specific category
+   * use sanity querying language from sanity db
+   */
+  useEffect(() => {
+    setLoading(true);
+    if (categoryId) {
+      const query = searchQuery(categoryId);
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [categoryId]); // to be called anytime that our category changes
+
+  if (loading)
+    return <Spinner message="We are adding news ideas to your feed!" />;
+
+  return <div>{pins && <MasonryLayout pins={pins} />}</div>;
+};
+
+export default Feed;
