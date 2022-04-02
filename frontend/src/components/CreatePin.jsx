dd0f9e7b20aad3d546671b437f8536433760ba76
PhyoWaiNyunt <50352633+pwn-69@users.noreply.github.com>
2022-04-02 22:52:27 +0630

    create-pin started

--- frontend/src/components/CreatePin.jsx
+++ frontend/src/components/CreatePin.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React, { useState } from "react";
+import { AiOutlineCloudUpload } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
+import { MdDelete } from "react-icons/md";
 
-const CreatePin = () => {
+import { categories } from "../utils/data";
+import { client } from "../client";
+import Spinner from "./Spinner";
+
+const CreatePin = ({ user }) => {
+  const [title, setTitle] = useState("");
+  const [about, setAbout] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [destination, setDestination] = useState();
+  const [fields, setFields] = useState(false);
+  const [category, setCategory] = useState();
+  const [imageAsset, setImageAsset] = useState();
+  const [wrongImageType, setWrongImageType] = useState(false);
+
+  const navigate = useNavigate();
+
+  const uploadImage = (e) => {
+    const { type } = e.target.files[0];
+
+    if (
+      type === "image/png" ||
+      type === "image/svg" ||
+      type === "image/jpeg" ||
+      type === "image/gif" ||
+      type === "image/tiff"
+    ) {
+      setWrongImageType(false);
+      setLoading(true);
+    } else {
+      setWrongImageType(true);
+    }
+  };
+
   return (
-    <div>CreatePin</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center mt-5 lg:h-4/5">
+      {fields && (
+        <p className="mb-5 text-xl text-red-500 transition-all duration-150 ease-in">
+          Please fill in all the fields.
+        </p>
+      )}
+      <div className="flex flex-col items-center justify-center w-full p-3 bg-white lg:flex-row lg:p-5 lg:w-4/5">
+        <div className="bg-secondaryColor p-3 flex flex-0.7 w-full">
+          <div className="flex flex-col items-center justify-center w-full p-3 border-2 border-gray-300 border-dotted h-420">
+            {loading && <Spinner />}
+            {wrongImageType && <p>Wrong Image Type!</p>}
+            {!imageAsset ? (
+              <label>
+                <div className="flex flex-col items-center justify-center h-full">
+                  <div className="flex flex-col items-center justify-center">
+                    <p className="text-2xl font-bold">
+                      <AiOutlineCloudUpload />
+                    </p>
+                    <p className="text-lg">Click to upload</p>
+                  </div>
+                  <p className="mt-32 text-gray-400">
+                    Use high-quality JPG, SVG, PNG, GIF or TIFF less than 20 MB
+                  </p>
+                </div>
+                <input
+                  type="file"
+                  name="upload-image"
+                  onChange={uploadImage}
+                  className="w-0 h-0"
+                />
+              </label>
+            ) : (
+              <p>something else</p>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CreatePin
+export default CreatePin;
